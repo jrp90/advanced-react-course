44ab338d2a01d1a8d83f85d492448f46a9e20103
James Pettifer <james@carsnip.com>
2018-12-26 20:47:26 +0000

    lessons 16-20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -12,6 +12,36 @@ const Mutation = {
     );
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // Take a copy of the updates
+    const updates = { ...args };
+
+    // Remove the ID from the updates
+    delete updates.id;
+
+    // Run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    //Find the Item
+    const item = await ctx.db.query.item({ where }, `{ id, title}`);
+
+    //Check if they own the item, or have the permissions
+    // TODO
+
+    //Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 

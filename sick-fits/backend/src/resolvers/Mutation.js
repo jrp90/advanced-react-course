a8c27d615ca8bc5f84a4236d50a0dc73ae552f58
James Pettifer <james@carsnip.com>
2018-12-27 11:52:53 +0000

    Lessons 21-24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -42,6 +45,28 @@ const Mutation = {
 
     //Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+
+    // hash their password
+    const password = await bycrypt.hash(args.password, 10);
+
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      { data: { ...args, password, permissions: { set: ['USER'] } } },
+      info
+    );
+
+    // create a JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // Set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 });
+
+    // Return user to browser
+    return user;
   }
 };
 
--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Pagination from './Pagination';
 
 import Item from './Item';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,7 +36,13 @@ export default class Items extends Component {
   render() {
     return (
       <Center>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -48,6 +56,7 @@ export default class Items extends Component {
             );
           }}
         </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }

6fcc57f85947a34a18ec4f2a0c81e693d537abfb
James Pettifer <james@carsnip.com>
2018-12-28 12:05:16 +0000

    Lessons 25-33

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,24 @@
 const bycrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutation = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This is how we create a releationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args
         }
       },
@@ -67,6 +78,95 @@ const Mutation = {
 
     // Return user to browser
     return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // first check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    //check if there password is correct
+    const valid = await bycrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password!');
+    }
+
+    // generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // Set JWT Cookie
+    ctx.response.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 });
+
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  },
+  async requestReset(parent, args, ctx, info) {
+    // check if this is a real user
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${args.email}`);
+    }
+
+    // set a reset token and expiry
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+
+    // email them that token
+    const mailResponse = await transport.sendMail({
+      from: 'james@jamespettifer.com',
+      to: user.email,
+      subject: 'Your Pasword Reset Token',
+      html: makeANiceEmail(`
+        Your Password Reset Token is here!
+        \n\n
+        <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to Reset</a>
+      `)
+    });
+
+    // return the message
+    return { message: 'Thanks!' };
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // check if the passwords match
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Yo passwords don't match");
+    }
+
+    // check if it's a legit reset token
+    // check if it's expired
+    const [user] = await ctx.db.query.users({
+      where: { resetToken: args.resetToken, resetTokenExpiry_gte: Date.now() - 3600000 }
+    });
+    if (!user) {
+      throw new Error('This token is either invalid or expired!');
+    }
+
+    // hash their new password
+    const password = await bycrypt.hash(args.password, 10);
+
+    // save the new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: { password, resetToken: null, resetTokenExpiry: null }
+    });
+
+    // Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+
+    // Set cookie
+    ctx.response.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 });
+
+    // Return new user
+    return updatedUser;
   }
 };
 
